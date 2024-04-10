86685b113f1b2d740b02031c9e785671d3f38c6f
Felipe Firmino <lipe19x@gmail.com>
2024-04-09 22:24:06 -0300

    commit app routes funcionando
    - produtos.txt atualizado
    - app routes disponiveis

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import json
 
+
 app = Flask(__name__)
 
 
@@ -14,7 +15,6 @@ def ler_arquivo(arquivo):
             if linha:
                 try:
                     produto = json.loads(linha)
-                    # Adicionar valores para cálculo de lucratividade e reabastecimento
                     produto['preco_compra'] = produto['preco_compra']  
                     produto['preco_venda'] = produto['preco_venda'] 
                     produto['quantidade_vendida'] = produto['media_vendas']
@@ -23,8 +23,13 @@ def ler_arquivo(arquivo):
                     print(f"Aviso: Ignorando linha mal formatada no arquivo: {linha}")
     return produtos
 
+def listar_produtos(produtos):
+    print("Lista de Produtos:")
+    for idx, produto in enumerate(produtos, 1):
+        print(f"{idx}. {produto['nome']}")
 
-
+        
+# ALTERAR CAMINHO AQUI
 arquivo = 'C:/Users/lipe7/OneDrive/Documentos/GitHub/Backend_youStore/produtos.txt'  # nome do arquivo txt
 produtos = ler_arquivo(arquivo)
 
@@ -32,6 +37,11 @@ produtos = ler_arquivo(arquivo)
 @app.route('/listar_produtos', methods=['GET'])
 def listar_produtos():
     return jsonify(produtos)
+
+@app.route('/ler_arquivo', methods=['GET'])
+def ler_arquivo():
+    return jsonify(produtos)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
