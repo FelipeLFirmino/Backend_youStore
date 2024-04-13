890148234ac5e02bb0d0839264c0a859a0b33864
Felipe Firmino <lipe19x@gmail.com>
2024-04-13 19:01:27 -0300

    commit 13/04
    - Modelo random forest quase treinado
    -Pasta data criada e inserido novos arquivos txt simulando meses
    -INSTRUÇÕES ADICIONADAS NOS ARQUIVOS

--- flask-server/functions/ler_arquivo.py
+++ flask-server/functions/ler_arquivo.py
@@ -1,5 +1,6 @@
 import json
-
+# importar essa funcao no server.py e fazer com que ela leia todos os txt
+# LEMBRANDO QUE ESSA FUNÇÃO PRECISA IMPORTAR OS ARQUIVOS DE TEXTO e SEUS RESPECTIVOS CAMINHOS (ELES ESTAO NA PASTA DATA AGR)
 def ler_arquivo(arquivo):
     produtos = []
     with open(arquivo, 'r', encoding='utf-8') as file:
--- flask-server/server.py
+++ flask-server/server.py
@@ -5,35 +5,8 @@ import json
 app = Flask(__name__)
 
 
-
-
-def ler_arquivo(arquivo):
-    produtos = []
-    with open(arquivo, 'r', encoding='utf-8') as file:
-        for linha in file:
-            linha = linha.strip()
-            if linha:
-                try:
-                    produto = json.loads(linha)
-                    produto['preco_compra'] = produto['preco_compra']  
-                    produto['preco_venda'] = produto['preco_venda'] 
-                    produto['quantidade_vendida'] = produto['media_vendas']
-                    produtos.append(produto)
-                except json.JSONDecodeError:
-                    print(f"Aviso: Ignorando linha mal formatada no arquivo: {linha}")
-    return produtos
-
-def listar_produtos(produtos):
-    print("Lista de Produtos:")
-    for idx, produto in enumerate(produtos, 1):
-        print(f"{idx}. {produto['nome']}")
-
-        
-# ALTERAR CAMINHO AQUI
-arquivo = 'C:/Users/lipe7/OneDrive/Documentos/GitHub/Backend_youStore/produtos.txt'  # nome do arquivo txt
-produtos = ler_arquivo(arquivo)
-
-
+# essas sao as rotas do backend elas devem ser criadas com nomes intuitivos e as funções importadas colocadas aqui
+# NAO COLOCAR FUNÇOES DIRETAMENTE NESSE ARQUIVO
 @app.route('/listar_produtos', methods=['GET'])
 def listar_produtos():
     return jsonify(produtos)
