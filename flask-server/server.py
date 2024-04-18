c0938713e2a60dfcbcc149bafaf93fbb947266df
Felipe Firmino <lipe19x@gmail.com>
2024-04-17 23:16:04 -0300

    commit aleatorio

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import json
+from functions import ler_arquivos
 
 
 app = Flask(__name__)
@@ -7,14 +7,48 @@ app = Flask(__name__)
 
 # essas sao as rotas do backend elas devem ser criadas com nomes intuitivos e as funções importadas colocadas aqui
 # NAO COLOCAR FUNÇOES DIRETAMENTE NESSE ARQUIVO
-@app.route('/listar_produtos', methods=['GET'])
-def listar_produtos():
-    return jsonify(produtos)
+# @app.route('/listar_produtos', methods=['GET'])
+# def listar_produtos():
+#     return jsonify(produtos)
 
-@app.route('/ler_arquivo', methods=['GET'])
-def ler_arquivo():
-    return jsonify(produtos)
+ # Pasta onde estão os arquivos JSON
+pasta = 'C:/Users/lipe7/OneDrive/Documentos/GitHub/Backend_youStore/Data'
 
+@app.route('/caminho_ler_arquivos', methods=['GET'])
+def caminho_ler_arquivos():
+    return ler_arquivos(pasta)
+
+
+
+
+
+
+
+
+
+
+
+
+# Adicionando novas approute aqui
+# @app.route('/calcular_media_vendas', methods=['GET'])
+# def calcular_media_vendas_route():
+#     media_vendas = calcular_media_vendas(produtos)
+#     return jsonify(media_vendas)
+
+# @app.route('/calcular_lucratividade', methods=['POST'])
+# def calcular_lucratividade_route():
+#     dados = request.get_json()
+#     preco_compra = dados['preco_compra']
+#     preco_venda = dados['preco_venda']
+#     demanda = dados['demanda']
+#     media_vendas = dados['media_vendas']
+    
+#     lucratividade = calcular_lucratividade(preco_compra, preco_venda, demanda, media_vendas)
+    
+#     return jsonify({'lucratividade': lucratividade})
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+#teste
