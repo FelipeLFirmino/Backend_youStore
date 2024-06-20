686edcf159f74b8232a9240c52cac19f7d021041
Bruno Sandes <brunosandes04@gmail.com>
2024-06-20 00:30:01 -0300

    enhancement
    my version of trying to fix the random forest algorithm related to the database

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 from functions import ler_arquivos, dados_mesas, dados_computadores, dados_teclados, dados_lixeiras, dados_cadeiras
 from randomForest import randomforestmodel
+from decimal import Decimal
 import psycopg2
 import psycopg2.extras
 
-
-#CONEXÃO BANCO DE DADOS
+# Função para obter conexão com o banco de dados
 def get_connection():
     return psycopg2.connect(
         host='you-store.c7k26sugsa4x.us-east-1.rds.amazonaws.com',
@@ -16,12 +16,14 @@ def get_connection():
         port=5432
     )
 
-
 app = Flask(__name__)
 
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    raise TypeError
 
-
-#VER TODOS OS PRODUTOS
+# Rota para ler todos os produtos
 @app.route('/caminho_ler_produtos', methods=['GET'])
 def caminho_ler_produtos():
     connection = None
@@ -39,6 +41,7 @@ def caminho_ler_produtos():
             connection.close()
     return jsonify(result)
 
+# Função para ler os dados de um produto específico do banco de dados
 def ler_dados_produto(nome_produto):
     connection = None
     try:
@@ -55,7 +58,7 @@ def ler_dados_produto(nome_produto):
             connection.close()
     return result
 
-#REQUISIÇÕES PRODUTOS
+# Rotas para ler dados de produtos específicos
 @app.route('/caminho_ler_mesas', methods=['GET'])
 def caminho_ler_mesas():
     dados_json_mesa = ler_dados_produto('Mesa')
@@ -81,150 +84,63 @@ def caminho_ler_cadeiras():
     dados_json_cadeira = ler_dados_produto('Cadeira')
     return jsonify(dados_json_cadeira)
 
-
-
+# Rota para prever a demanda de um produto específico
 @app.route('/caminho_prever_demanda_produto/<string:produto>', methods=['GET'])
 def caminho_prever_demanda_produto(produto):
     if produto not in ['Computador', 'Mesa', 'Cadeira', 'Teclado', 'Lixeira']:
-        return jsonify({'erro': 'Produto não reconhecido!'})
+        return jsonify({'erro': 'Produto não reconhecido!'}), 400
 
-    dados_produto = ler_dados_produto(produto)
-    previsao = randomforestmodel.prever_media_vendas(json.dumps(dados_produto))  # Use json.dumps para converter para JSON
-
-    return jsonify({'previsao': previsao})  # Retorna como dicionário JSON
-
+    try:
+        # Não é necessário ler dados do produto do request.json, pois o produto já está na URL
+        # dados_produto = request.json
+        # dados_produto_json = json.dumps(dados_produto)
+        
+        # Ler os dados específicos do produto do banco de dados
+        dados_produto = ler_dados_produto(produto)
+        #dados_produto_json = json.dumps(dados_produto)
+        
+        # Prever a demanda usando o modelo
+        previsao = randomforestmodel.prever_media_vendas(dados_produto) #(dados_produto_json)
+        
+        return jsonify({'previsao': json.loads(previsao)}), 200
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 500
+    
 @app.route('/caminho_prever_demanda_produto/todos_produtos', methods=['GET'])
 def caminho_prever_demanda_todos_produtos():
     previsoes_todos_produtos = {}
 
-    for produto in ['Computador', 'Mesa', 'Cadeira', 'Teclado', 'Lixeira']:
-        dados_produto = ler_dados_produto(produto)
-        previsao = randomforestmodel.prever_media_vendas(json.dumps(dados_produto))  # Use json.dumps para converter para JSON
-        previsoes_todos_produtos[produto] = previsao
+    try:
+        for produto in ['Computador', 'Mesa', 'Cadeira', 'Teclado', 'Lixeira']:
+            dados_produto = ler_dados_produto(produto)
+            #dados_produto_json = json.dumps(dados_produto, default=decimal_default)
+            previsao = randomforestmodel.prever_media_vendas(dados_produto) #(dados_produto_json)
 
-    return jsonify(previsoes_todos_produtos)
+            previsoes_todos_produtos[produto] = previsao
+
+        return jsonify(previsoes_todos_produtos), 200
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 500
+
+# Rota para prever a demanda de todos os produtos
+# @app.route('/caminho_prever_demanda_produto/todos_produtos', methods=['GET'])
+# def caminho_prever_demanda_todos_produtos():
+#     previsoes_todos_produtos = {}
+
+#     try:
+#         for produto in ['Computador', 'Mesa', 'Cadeira', 'Teclado', 'Lixeira']:
+#             dados_produto = ler_dados_produto(produto)
+#             dados_produto_json = json.dumps(dados_produto)
+#             previsao = randomforestmodel.prever_media_vendas(dados_produto_json)
+
+#             # Converter valores Decimal para float antes de serializar para JSON
+#             previsao_convertida = {k: float(v) if isinstance(v, Decimal) else v for k, v in previsao.items()}
+
+#             previsoes_todos_produtos[produto] = previsao_convertida
+
+#         return jsonify(previsoes_todos_produtos), 200
+#     except Exception as e:
+#         return jsonify({'erro': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# essas sao as rotas do backend elas devem ser criadas com nomes intuitivos e as funções importadas colocadas aqui
-# NAO COLOCAR FUNÇOES DIRETAMENTE NESSE ARQUIVO
-
-# pasta = 'C:/Users/lipe7/OneDrive/Documentos/GitHub/Backend_youStore/Data'
-
-# @app.route('/caminho_ler_arquivos', methods=['GET'])
-# def caminho_ler_arquivos():
-#     dados_json = ler_arquivos.ler_arquivo(pasta)
-#     return jsonify(dados_json)
-
-
-# @app.route('/caminho_ler_mesas', methods=['GET'])
-# def caminho_ler_mesas():
-#     dados_json = ler_arquivos.ler_arquivo(pasta)
-#     dados_json_mesa = dados_mesas.dados_mesas(dados_json)
-#     return jsonify(dados_json_mesa)
-
-# @app.route('/caminho_ler_computadores', methods=['GET'])
-# def caminho_ler_computadores():
-#     dados_json = ler_arquivos.ler_arquivo(pasta)
-#     dados_json_computadores = dados_computadores.dados_computadores(dados_json)
-#     return jsonify(dados_json_computadores)
-
-# @app.route('/caminho_ler_teclados', methods=['GET'])
-# def caminho_ler_teclados():
-#     dados_json = ler_arquivos.ler_arquivo(pasta)
-#     dados_json_teclado = dados_teclados.dados_teclados(dados_json)
-#     return jsonify(dados_json_teclado)
-
-# @app.route('/caminho_ler_lixeiras', methods=['GET'])
-# def caminho_ler_lixeiras():
-#     dados_json = ler_arquivos.ler_arquivo(pasta)
-#     dados_json_lixeira = dados_lixeiras.dados_lixeiras(dados_json)
-#     return jsonify(dados_json_lixeira)
-
-# @app.route('/caminho_ler_cadeiras', methods=['GET'])
-# def caminho_ler_cadeiras():
-#     dados_json = ler_arquivos.ler_arquivo(pasta)
-#     dados_json_cadeira = dados_cadeiras.dados_cadeiras(dados_json)
-#     return jsonify(dados_json_cadeira)
-
-
-# @app.route('/caminho_prever_demanda_produto/<string:produto>', methods=['GET'])
-# def caminho_prever_demanda_produto(produto):
-#     # Verificar se o produto fornecido está na lista de produtos permitidos
-#     if produto not in ['computador', 'mesa', 'cadeira', 'teclado', 'lixeira']:
-#         return jsonify({'erro': 'Produto não reconhecido!'})
-
-#     # Ler os dados do arquivo correspondente ao produto
-#     dados_json = ler_arquivos.ler_arquivo(pasta) 
-    
-#     # Selecionar os dados específicos do produto
-#     if produto == 'computador':
-#         dados_produto = dados_computadores.dados_computadores(dados_json)
-#     elif produto == 'mesa':
-#         dados_produto = dados_mesas.dados_mesas(dados_json)
-#     elif produto == 'cadeira':
-#         dados_produto = dados_cadeiras.dados_cadeiras(dados_json)
-#     elif produto == 'teclado':
-#         dados_produto = dados_teclados.dados_teclados(dados_json)
-#     elif produto == 'lixeira':
-#         dados_produto = dados_lixeiras.dados_lixeiras(dados_json)
-
-#     # Treinar o modelo usando os dados do produto e obter a previsão
-#     previsao = randomforestmodel.prever_media_vendas(dados_produto)
-
-#     # Retornar a previsão para o produto específico
-#     return jsonify({f'previsao_{produto}': previsao})
-
-
-# @app.route('/caminho_prever_demanda_produto/todos_produtos', methods=['GET'])
-# def caminho_prever_demanda_todos_produtos():
-#     # Lista para armazenar as previsões de demanda de todos os produtos
-#     previsoes_todos_produtos = {}
-
-#     # Fazendo previsões de demanda para cada produto individualmente
-#     for produto in ['computador', 'mesa', 'cadeira', 'teclado', 'lixeira']:
-#         # Ler os dados do arquivo correspondente ao produto
-#         dados_json = ler_arquivos.ler_arquivo(pasta)
-
-#         # Selecionar os dados específicos do produto
-#         if produto == 'computador':
-#             dados_produto = dados_computadores.dados_computadores(dados_json)
-#         elif produto == 'mesa':
-#             dados_produto = dados_mesas.dados_mesas(dados_json)
-#         elif produto == 'cadeira':
-#             dados_produto = dados_cadeiras.dados_cadeiras(dados_json)
-#         elif produto == 'teclado':
-#             dados_produto = dados_teclados.dados_teclados(dados_json)
-#         elif produto == 'lixeira':
-#             dados_produto = dados_lixeiras.dados_lixeiras(dados_json)
-
-#             # Treinar o modelo usando os dados do produto e obter a previsão
-#         previsao = randomforestmodel.prever_media_vendas(dados_produto)
-
-#         # Adicionando a previsão do produto à lista
-#         previsoes_todos_produtos[produto] = previsao
-
-#     # Retornando as previsões de todos os produtos
-#     return jsonify(previsoes_todos_produtos)
-
-
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-# #teste
